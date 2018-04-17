8046b3eca2883c9958af381eceeb04549aaf3979
fzheng1 <kaifenzheng@gmail.com>
2018-04-17 08:47:31 -0400

    New bookstore controller

---
+++ app/controllers/BookStoreController.java
@@ -0,0 +1,42 @@
+package app.controllers;
+
+import play.mvc.Controller;
+
+import play.mvc.Result;
+
+public class BookStoreController extends Controller {
+
+    // showing all books to user
+    public Result index(){
+        return TODO;
+    }
+
+    // to create book
+    public Result create(){
+        return TODO;
+    }
+
+    // save book
+    public Result save(){
+        return TODO;
+    }
+
+    // edit book
+    public Result edit(int id){
+        return TODO;
+    }
+
+    public Result update(){
+        return TODO;
+    }
+
+    public Result destroy(int id){
+        return TODO;
+    }
+
+    // for book details
+    public Result show(int id){
+        return TODO;
+    }
+
+}
